3e4b5960c18542bafa6bad97d17a9e1106ccfce5
biswasuser <user.lohanibiswas1@gmail.com>
2025-06-02 13:26:31 +0545

    created banner section

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-import { doctors } from "../assets/assets";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 const TopDoctors = () => {
+  const {doctors} = useContext(AppContext)
+
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-500 md:mx-10">
       <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
@@ -12,7 +17,7 @@ const TopDoctors = () => {
       {/* Responsive Grid Template */}
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 pt-5 gap-y-6 px-3 sm:px-0">
         {doctors.slice(0, 10).map((doctor, index) => (
-          <div
+          <div onClick={() => navigate(`/appointment/${doctor._id}`)}
             key={index}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
           >
@@ -33,7 +38,7 @@ const TopDoctors = () => {
         ))}
       </div>
 
-      <button className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10 hover:bg-blue-100 transition-colors">
+      <button onClick={() => {navigate('/doctors'); scrollTo(0,0)}} className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10 hover:bg-blue-100 transition-colors">
         More
       </button>
     </div>
