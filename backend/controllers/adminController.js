5156baf97d2669cfbec08069bdc7b6709b860021
biswasuser <user.lohanibiswas1@gmail.com>
2025-06-05 21:30:54 +0545

    completed solving bugs

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -4,11 +4,11 @@ import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
 
 // for adding doctor
-const addDoctor = async () => {
+const addDoctor = async (req, res) => {
 
     try {
         const {name, email, password, speciality, degree,experience,about,fees, address} = req.body;
-        const imageFile = req.image.file
+        const imageFile = req.file
        
         // checking for all daa to add doctor
         if (!name || !email || !password || !speciality || !degree || ! experience || !about || !fees || !address) {
@@ -21,7 +21,7 @@ const addDoctor = async () => {
         }
 
         // validating strong password
-        if (!password.length < 8 ) {
+        if (password.length < 8 ) {
              return res.json({success: false, message: "Please enter a strong password"}) 
         }
         
@@ -52,7 +52,7 @@ const addDoctor = async () => {
         res.json({success: true, message: "Doctor Added"})
 
     } catch (error) {
-        console.log(message);
+        console.log(error.message);
           res.json({success: false, message: error.message})
         
         
