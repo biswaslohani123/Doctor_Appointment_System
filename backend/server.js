b209e13333cd9c54abea50900ca93c1b37911eec
biswasuser <user.lohanibiswas1@gmail.com>
2025-06-05 17:23:09 +0545

    sucessfully connected database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDb from './config/mongodb.js';
 
 //app config
 const app = express();
 const port = process.env.PORT || 4000
+connectDb()
 
 //middlewares
 app.use(express.json())
