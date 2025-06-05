85b09ea46c02327e46aedad54c8760b2a0a726ea
biswasuser <user.lohanibiswas1@gmail.com>
2025-06-05 10:34:17 +0545

    mde logic for isedit name

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -15,44 +15,19 @@ const MyProfile = () => {
     gender: 'Male',
     dob: '2004-01-20'
   })
+
   const [isEdit, setIsEdit] = useState(false)
 
   return (
-    <div>
-      <img src={userData.image} alt="" />
-      {
-        isEdit ? <input type="text" value={userData.name} onChangeCapture={e => setUserData(prev => ({...prev,name: e.target.value}))} />
-        : <p>{userData.name}</p>
-      }
+   <div>
 
-      <hr />
-      <div>
-        <p>CONTACT INFORMATION</p>
-        <div>
-          <p>Email id:</p>
-          <p>{userData.email}</p>
-          <p>Phone:</p>
-           {
-        isEdit ? <input type="text" value={userData.phone} onChange={e => setUserData(prev => ({...prev,phone: e.target.value}))} />
-        : <p>{userData.phone}</p>
-      }
-      <p>Address:</p>
-      {
-        isEdit 
-        ? <p>
-        <input type="text" onChange={(e) => setUserData(prev => ({...prev, address: {...prev.address, line1: e.target.value}}))} value={userData.address.line1} />
-        <br />
-        <input type="text" onChange={(e) => setUserData(prev => ({...prev, address: {...prev.address, line2: e.target.value}}))} value={userData.address.line2} /></p>
-        :
-        <p>{userData.address.line1}
-        <br />
-        <p>{userData.address.line2}</p>
-        </p>
-      }
-        </div>
-      </div>
-      <p>BASIC INFORMATIONS</p>
-    </div>
+    <img src={assets.profile_pic} alt="" />
+    {
+      isEdit ? <input type="text" value={userData.name} onChange={e => setUserData(prev => ({...prev, name:e.target.value}))} />: <p>{userData.name}</p>
+    }
+
+
+   </div>
   )
 }
 
