efc3dee4842a84b0821a751f91e3a381163c853e
biswasuser <user.lohanibiswas1@gmail.com>
2025-06-06 19:19:34 +0545

    completed UI for add doctor page

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
     <div className="min-h-screen bg-white border-r">
       {atoken && (
         <ul className="text-[#515151] mt-5">
-          <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-blue-400': ''} `} to='/admin-dashboard'>
+          <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-10 border-blue-400': ''} `} to='/admin-dashboard'>
             <img src={assets.home_icon} alt="" /> <p>Dashboard</p>
           </NavLink>
           <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r border-blue-400': ''} `} to='/all-appointments'>
