166e6832cd3ee682cb52a2b5313c841832fa4c2f
biswasuser <user.lohanibiswas1@gmail.com>
2025-06-06 11:15:55 +0545

    sucessfully created authAdmin middleware

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { addDoctor, loginAdmin } from '../controllers/adminController.js'
 import upload from '../config/multer.js'
+import authAdmin from '../middleware/authAdmin.js';
 
 
 const adminRouter = express.Router()
 
-adminRouter.post('/add-doctor',upload.single("image"), addDoctor);
+adminRouter.post('/add-doctor',authAdmin,upload.single("image"), addDoctor);
 adminRouter.post('/admin-login',loginAdmin)
 
 export default adminRouter
