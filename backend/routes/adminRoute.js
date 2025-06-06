8e09e05b2c7791a9b60e46d2cb4f3f509a19d6d8
biswasuser <user.lohanibiswas1@gmail.com>
2025-06-06 11:10:06 +0545

    Sucessfully tested login admin and also tested

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { addDoctor } from '../controllers/adminController.js'
+import { addDoctor, loginAdmin } from '../controllers/adminController.js'
 import upload from '../config/multer.js'
 
 
 const adminRouter = express.Router()
 
-adminRouter.post('/add-doctor',upload.single("image"), addDoctor)
+adminRouter.post('/add-doctor',upload.single("image"), addDoctor);
+adminRouter.post('/admin-login',loginAdmin)
 
 export default adminRouter
