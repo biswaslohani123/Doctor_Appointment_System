397c68edfe4f3b42db464fba61bb172a1c235208
biswasuser <user.lohanibiswas1@gmail.com>
2025-06-07 17:49:40 +0545

    completed backend fro profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
+import {v2 as cloudinary} from 'cloudinary'
 
 // Api to register User
 const registerUser = async (req, res) => {
@@ -88,7 +89,34 @@ const getProfile = async (req, res) => {
 
 //To update user Profile
 const updateProfile = async (req, res) => {
-  
+  try {
+
+    const {userId, name, phone, address, dob, gender} = req.body
+    const imageFile = req.file
+
+    if (!name || !phone || !dob || !gender) {
+      return res.json({success: false, message: "Data Missing"})
+    }
+
+    await userModel.findByIdAndUpdate(userId, {name, phone, address:JSON.parse(address), dob,gender})
+
+    if (imageFile) {
+
+      // upload image to cloudinary
+      const imageUpload =  await cloudinary.uploader.upload(imageFile.path,{resource_type: 'image'})
+      const imageURL = imageUpload.secure_url
+
+      await userModel.findByIdAndUpdate(userId,{image:imageURL})
+      
+    }
+    res.json({success: true, message: "Profile Updated"})
+
+
+    
+  } catch (error) {
+     console.log(error.message);
+     res.json({ success: false, message: error.message });
+  }
 }
 
-export { registerUser, loginUser, getProfile };
+export { registerUser, loginUser, getProfile, updateProfile };
