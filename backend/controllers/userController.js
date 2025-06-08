5d22933cd3ee3bfa593bb5a61de10581ad4e6a3d
biswasuser <lohanibiswas1@gmail.com>
2025-06-08 20:46:51 +0545

    sucessfully created controller for cancel-appointment and made route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -188,7 +188,45 @@ const listAppointment = async (req, res) => {
     }
 }
 
+// cancel Appointment for user
+const cancelAppointment = async (req, res) => {
+    try {
+
+        const userId = req.userId;
+        const {appointmentId} = req.body;
+
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+        // verify appointment user
+        if (appointmentData.userId !== userId) {
+            return res.json({success: false, message: "Unauthorized Action"})
+        }
+
+        await appointmentModel.findByIdAndUpdate(appointmentId,{cancelled: true} )
+
+        // releasing Doctor Slot
+         
+        const {docId, slotDate, slotTime} = appointmentData
+
+        const doctorData = await doctorModel.findById(docId)
+
+        let slots_booked = doctorData.slots_booked
+
+        slots_booked[slotDate] = slots_booked[slotDate].filter(e => e != slotTime)
+
+        await doctorModel.findByIdAndUpdate(docId, {slots_booked})
+
+        res.json({success: true, message: "Appointment Cancelled"})
+
+
+        
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
 
 
 
-export { registerUser, loginUser, getProfile, updateProfile,bookAppointment, listAppointment };
+
+export { registerUser, loginUser, getProfile, updateProfile,bookAppointment, listAppointment, cancelAppointment };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { bookAppointment, getProfile, listAppointment, loginUser, registerUser, updateProfile } from '../controllers/userController.js'
+import { bookAppointment, cancelAppointment, getProfile, listAppointment, loginUser, registerUser, updateProfile } from '../controllers/userController.js'
 import authUser from '../middleware/userAuth.js'
 import upload from '../config/multer.js'
 
@@ -11,5 +11,6 @@ userRouter.get('/get-profile', authUser,getProfile);
 userRouter.post('/update-profile',upload.single('image'),authUser,updateProfile)
 userRouter.post('/book-appointment', authUser, bookAppointment)
 userRouter.get('/list-appointment', authUser, listAppointment)
+userRouter.post('/cancel-appointment',authUser, cancelAppointment)
 
 export default userRouter
