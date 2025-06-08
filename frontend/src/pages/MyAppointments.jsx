9411854b3d5cfa66a522c5e2ff536a8a14039ca8
biswasuser <lohanibiswas1@gmail.com>
2025-06-08 20:58:23 +0545

    completed  fully

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -90,6 +90,9 @@ const MyAppointments = () => {
                {
                 !item.cancelled && <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border  hover:bg-red-600 hover:text-white transition-all duration-300 cursor-pointer'>Cancel Appointment</button>
                }
+               {
+                item.cancelled && <button className='sm:min-w-48 py-2 px-2 border border-red-500 rounded text-red-500 '>Appointment Cancelled</button>
+               }
                
               </div>
           </div>
