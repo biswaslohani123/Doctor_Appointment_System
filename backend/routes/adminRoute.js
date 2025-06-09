c8a7cf3f72f719990da8f56367c15a9bc09089db
biswasuser <lohanibiswas1@gmail.com>
2025-06-09 10:21:40 +0545

    made route

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addDoctor, AllDoctors, appointmentAdmin, loginAdmin } from '../controllers/adminController.js'
+import { addDoctor, AllDoctors, appointmentAdmin, cancelAppointment, loginAdmin } from '../controllers/adminController.js'
 import upload from '../config/multer.js'
 import authAdmin from '../middleware/authAdmin.js';
 import { changeAvailability } from '../controllers/doctorController.js';
@@ -12,5 +12,6 @@ adminRouter.post('/admin-login',loginAdmin);
 adminRouter.post('/doctor-list',authAdmin, AllDoctors)
 adminRouter.post('/change-availability',authAdmin, changeAvailability)
 adminRouter.get('/get-appointment',authAdmin, appointmentAdmin)
+adminRouter.post('/cancel-appointment', authAdmin, cancelAppointment)
 
 export default adminRouter
